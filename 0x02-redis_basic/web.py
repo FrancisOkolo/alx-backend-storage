a5fad3fc5122e188cba5e83cbdfcd1dbe8c679ff
FrancisOkolo <francischinedu78@gmail.com>
2023-10-18 21:59:56 +0000

    redis task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,45 @@
 #!/usr/bin/env python3
-"""Implementing an expiring web cache and tracker with Redis"""
-import redis
-import requests
-from functools import wraps
-from typing import Callable
-
-
-redis_store = redis.Redis()
-"""redis storage variabale
+"""In this task, we will implement a get_page function
+(prototype: def get_page(url: str) -> str:). The core of
+the function is very simple. It uses the requests module
+to obtain the HTML content of a particular URL and returns it.
 """
 
 
-def data_cacher(method: Callable) -> Callable:
-    """wrapper
-    """
+import redis
+import requests
+from functools import wraps
+
+r = redis.Redis()
+
+
+def url_access_count(method):
+    """decorator for get_page function"""
     @wraps(method)
-    def invoker(url) -> str:
-        """ wrapper
-        """
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def wrapper(url):
+        """wrapper function"""
+        key = "cached:" + url
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+            # Get new content and update cache
+        key_count = "count:" + url
+        html_content = method(url)
+
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        r.expire(key, 10)
+        return html_content
+    return wrapper
 
 
-@data_cacher
+@url_access_count
 def get_page(url: str) -> str:
-    """returns URL
-    """
-    return requests.get(url).text
+    """obtain the HTML content of a particular"""
+    results = requests.get(url)
+    return results.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
